0943461bfdfb046291270c1cba02807c210a83d4
Vitor <vitor.daniel@outlook.pt>
2023-04-29 17:28:12 +0100

    created BetterFillSpheres project and Color class with 2 constructs

---
+++ BetterFillSpheres/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace BetterFillSpheres
+{
+    /// <summary>
+    /// class Program para criar esferas usando dimensao e cor, e rebentar esferas
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    /// <summary>
+    /// class Color para definir 4 propriedades de cor
+    /// </summary>
+    public class Color
+    {
+        public byte Red { get; set; }
+        public byte Green { get; set; }
+        public byte Blue { get; set; }
+        public byte Alpha { get; set; }
+
+        //construtor de Color com as 4 propriedades por definir
+        public Color(byte red, byte green, byte blue, byte alpha)
+        {
+            Red = red;
+            Green = green;
+            Blue = blue;
+            Alpha = alpha;
+        }
+
+        //construtor de Color com 3 propriedades por definir e alpha definida
+        public Color(byte red, byte green, byte blue)
+        {
+            Red = red;
+            Green = green;
+            Blue = blue;
+            Alpha = 255;
+        }
+    }
+}
