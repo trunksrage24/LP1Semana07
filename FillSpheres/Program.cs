258e61e1e0533629136a293b1b180256e5ce62c8
Vitor <vitor.daniel@outlook.pt>
2023-04-29 15:36:53 +0100

    created class Color and 1st constructor

---
+++ FillSpheres/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace FillSpheres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    class Color
+    {
+        byte red
+        byte green
+        byte blue
+        byte alpha
+        
+        //colorize 4 paramenters
+        public byte Color (byte red, byte green, byte blue, byte alpha)
+        {
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
+            this.alpha = alpha;
+        }
+
+        //colorize 3 parameters
+        public byte Color (byte red, byte green, byte blue, byte alpha)
+        {
+            byte alpha = 255;
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
+        }
+    }
+
+    class Sphere
+    {
+       int color;
+       int radius;
+       int thrown;
+
+    }
+
+}
